c5f41959a51b21f5dbf55f6ce3dc34af48f02f55
Yash Mehta <yashmehta0395@gmail.com>
2023-09-12 16:07:04 +0530

    added announcements component from hyperui

---
+++ components/hyperui/announcements/announcementsFloatingButton.tsx
@@ -0,0 +1,16 @@
+const AnnouncementsFloatingButton = () => {
+    return ( 
+        <div className="fixed inset-x-0 bottom-0 p-4">
+  <div className="rounded-lg bg-indigo-600 px-4 py-3 text-white shadow-lg">
+    <p className="text-center text-sm font-medium">
+      Love Alpine JS?
+      <a href="#" className="inline-block underline">
+        Check out this new course!
+      </a>
+    </p>
+  </div>
+</div>
+     );
+}
+ 
+export default AnnouncementsFloatingButton;
---
+++ components/hyperui/announcements/announcementsWithAction.tsx
@@ -0,0 +1,22 @@
+const AnnouncementsWithAction = () => {
+    return ( 
+        <div
+  className="bg-indigo-600 px-4 py-3 text-white sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+>
+  <p className="text-center font-medium sm:text-left">
+    Love Alpine JS?
+    <br className="sm:hidden" />
+    Check out this new deep dive course!
+  </p>
+
+  <a
+    className="mt-4 block rounded-lg bg-white px-5 py-3 text-center text-sm font-medium text-indigo-600 transition hover:bg-white/90 focus:outline-none focus:ring active:text-indigo-500 sm:mt-0"
+    href="#"
+  >
+    Learn More
+  </a>
+</div>
+     );
+}
+ 
+export default AnnouncementsWithAction;
